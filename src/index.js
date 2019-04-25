bb420049504a6bf889bb0231216eae6194987c45
Lili Boxer <ninaliboxer@gmail.com>
2019-04-24 21:52:50 -0700

    flip panda event listener

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const winsResults = document.getElementById('wins');
+const lossesResuts = document.getElementById('losses');
+const messageResults = document.getElementById('message-results');
+const headsChoice = document.getElementById('heads-choice');
+const tailsChoice = document.getElementById('tails-choice');
+
+flipPanda.addEventListener('click', () => {
+
+    
+}); 
