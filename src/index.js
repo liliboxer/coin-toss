31d27811f1ce983e89e0c94e51178bfc798eb5b3
Lili Boxer <ninaliboxer@gmail.com>
2019-04-25 08:33:09 -0700

    wired up button click event and generated random number

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
+
+// reference the DOM elements you need
+const flipButton = document.getElementById('flip-button');
 const winsResults = document.getElementById('wins');
 const lossesResuts = document.getElementById('losses');
 const messageResults = document.getElementById('message-results');
 const headsChoice = document.getElementById('heads-choice');
 const tailsChoice = document.getElementById('tails-choice');
 
-flipPanda.addEventListener('click', () => {
+// initialize
 
+
+// subscribe - > add event listeners 
+flipButton.addEventListener('click', () => {
+    const randomNumber = Math.random();
+    console.log(randomNumber);
     
 }); 
+
