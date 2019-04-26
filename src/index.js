d60cf8e7d71d94f9fc4ccf8babc6ac0d6e31e721
Lili Boxer <ninaliboxer@gmail.com>
2019-04-25 21:06:40 -0700

    betting and wins/losses counter working

--- src/index.js
+++ src/index.js
@@ -7,17 +7,35 @@ const lossesResuts = document.getElementById('losses');
 const messageResults = document.getElementById('message-results');
 const headsChoice = document.getElementById('heads-choice');
 const tailsChoice = document.getElementById('tails-choice');
-
+const coinImage = document.getElementById('coin');
 
 // initialize
 
+let wins = 0;
+let losses = 0;
 
 // subscribe - > add event listeners 
 flipButton.addEventListener('click', () => {
     const randomNumber = Math.random();
     const flip = getHeadsOrTails(randomNumber);
+    
+    let guess = '';
+    if(headsChoice.checked) {
+        guess = 'heads';
+    } else {
+        guess = 'tails';
+    }
 
+    if(flip === guess) {
+        messageResults.textContent = 'You Won!';
+        wins ++;
+        winsResults.textContent = 'Wins: ' + wins;
 
+    } else {
+        messageResults.textContent = 'You Lost!';
+        losses ++;
+        lossesResuts.textContent = 'Loses: ' + losses;
+    }
 
 }); 
 
